cc04edc1eec2c80d176768933eed2a0ef712608c
HeiligeDuif <gerjanlogmans@gmail.com>
2026-03-30 03:15:23 +0200

    added location actions

--- consoleGame.cpp
+++ consoleGame.cpp
@@ -14,6 +14,13 @@
 #include <iomanip>
 #include <chrono>
 #include <thread>
+#include <limits>
+#include <unordered_map>
+
+#ifdef _WIN32
+#define NOMINMAX
+#include <windows.h>
+#endif
 
 //text colours
 const char* RED = "\033[31m";
@@ -47,8 +54,24 @@ struct enemy
 
 };
 
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(character, name, hpMax, attack)
+struct action
+{
+    std::string nameOfAction;
+    std::string resultOfAction;
+};
+
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(action, nameOfAction, resultOfAction)
+
+struct location
+{
+    std::string name = "name";
+    std::string description = "description";
+    std::vector<action> possibleActions;
+};
+
 NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(enemy, name, hpMax, attack)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(character, name, hpMax, attack)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(location, name, description, possibleActions)
 
 class choiceManager
 {
@@ -57,8 +80,9 @@ public:
     int amountOfChoices = 0;
     std::vector<char> charPossibilities;
 
-    std::vector<character> characters;
     std::vector<enemy> enemies;
+    std::vector<character> characters;
+    std::vector<location> locations;
 
     enemy currentCombatEnemy;
 
@@ -69,8 +93,10 @@ public:
 
     void startGame()
     {
+        setConsoleOutputUTF8();
         loadEnemies();
         loadCharacters();
+        loadLocations();
 
         setseed();
 
@@ -82,111 +108,36 @@ public:
     }
 
 private:
-    void setseed() 
+    void setConsoleOutputUTF8()
     {
-        std::cout << "custom seed?\n";
-        std::vector<std::string> yesOrNo =
-        {
-            "Yes",
-            "No"
-        };
-
-        amountOfChoices = yesOrNo.size();
-        vectorCreation();
-
-        for (int i = 0; i < yesOrNo.size(); i++)
-        {
-            std::cout << charPossibilities[i] << ". " << yesOrNo[i] << "\n";
-        }
-
-        if (correctInput() == 'A')
-        {
-            std::cout << "Enter seed (8 numbers)\n";
-            std::cin >> seedValue;
-        }else{
-            std::random_device dev;
-            std::mt19937 rng(dev());
-            std::uniform_int_distribution<std::mt19937::result_type> dist6(1, 99999999);
-
-            seedValue = dist6(rng);
-        }
-        std::cout << "Seed:" << std::setfill('0') << std::setw(8) << seedValue << "\n";
-        EMPTYSCREEN();
+#ifdef _WIN32
+        SetConsoleOutputCP(CP_UTF8);
+#endif
     }
 
-    void vectorCreation()
+    void printAscii(std::string fileName) {
+        std::string path = "graphics/" + fileName;
+        std::ifstream file(path);
+
+        if (!file.is_open()) {
+            std::cerr << "ERROR: Can't find " << path << " !" << std::endl;
+            return;
+        }
+
+        std::string line;
+        while (std::getline(file, line)) {
+            std::cout << line << "\n";
+        }
+
+        file.close();
+    }
+
+    void vectorCreation(size_t amountOfChoices)
     {
         charPossibilities.resize(amountOfChoices);
         for (int i = 0; i < amountOfChoices; i++)
         {
             charPossibilities[i] = static_cast<char>('A' + i);;
-        }
-    }
-
-    void loadCharacters()
-    {
-        std::ifstream file("characters.json");
-
-        if (!file.is_open()) {
-            throw std::runtime_error("Can't find JSON file!");;
-        }
-
-        json j;
-        file >> j;
-
-        characters = j.get<std::vector<character>>();
-        size_t amountOfCharacters = characters.size();
-        amountOfChoices = amountOfCharacters;
-        vectorCreation();
-
-        for (const auto& c : characters) {
-            std::cout << "Succesfully loaded characters: " << c.name << " (max HP: " << c.hpMax << ")" << "\n";
-            wait(20);
-        }
-        EMPTYSCREEN();
-    }
-
-    void setClass()
-    {
-        std::cout << "Choose a character:\n";
-        for (int i = 0; i < charPossibilities.size(); i++)
-        {
-            std::cout << charPossibilities[i]
-                << ". "
-                << characters[i].name << RED << " [ max HP: "
-                << characters[i].hpMax << BLUE <<" | ATK: "
-                << characters[i].attack << RESET << "]\n";;
-        }
-
-        char playerChoice = correctInput();
-
-        int characterChoiceInt = static_cast<int>(playerChoice) - 'A';
-        //std::cout << characterChoiceInt;
-
-        std::cout << "You chose: " << characters[characterChoiceInt].name << "\n";
-
-        player.name = characters[characterChoiceInt].name;
-        player.hpMax = characters[characterChoiceInt].hpMax;
-        player.attack = characters[characterChoiceInt].attack;
-
-		playerCurrentHP = player.hpMax;
-    }
-
-    void loadEnemies()
-    {
-        std::ifstream file("enemies.json");
-
-        if (!file.is_open()) {
-            throw std::runtime_error("Can't find JSON file!");;
-        }
-        json j;
-        file >> j;
-
-        enemies = j.get<std::vector<enemy>>();
-
-        for (const auto& c : enemies) {
-            std::cout << "Succesfully loaded enemies: " << c.name << " (max HP: " << c.hpMax << ")" << "\n";
-            wait(20);
         }
     }
 
@@ -220,27 +171,162 @@ private:
         } while (succesfulInput != true);
     }
 
+    void loadEnemies()
+    {
+        std::ifstream file("enemies.json");
+
+        if (!file.is_open()) {
+            throw std::runtime_error("Can't find JSON file!");;
+        }
+        json j;
+        file >> j;
+
+        enemies = j.get<std::vector<enemy>>();
+
+        for (const auto& c : enemies) {
+            std::cout << "Succesfully loaded enemies: " << c.name << " (max HP: " << c.hpMax << ")" << "\n";
+            wait(20);
+        }
+        EMPTYSCREEN();
+    }
+
+    void loadCharacters()
+    {
+        std::ifstream file("characters.json");
+
+        if (!file.is_open()) {
+            throw std::runtime_error("Can't find JSON file!");;
+        }
+
+        json j;
+        file >> j;
+
+        characters = j.get<std::vector<character>>();
+        size_t amountOfCharacters = characters.size();
+
+        vectorCreation(amountOfCharacters);
+
+        for (const auto& c : characters) {
+            std::cout << "Succesfully loaded characters: " << c.name << " (max HP: " << c.hpMax << ")" << "\n";
+            wait(20);
+        }
+        EMPTYSCREEN();
+    }
+
+    void loadLocations()
+    {
+        std::ifstream file("locations.json");
+
+        if (!file.is_open()) {
+            throw std::runtime_error("Can't find JSON file!");;
+        }
+
+        json j;
+        file >> j;
+
+        locations = j.get<std::vector<location>>();
+        size_t amountOfLocations = locations.size();
+
+        vectorCreation(amountOfLocations);
+
+        for (const auto& c : locations) {
+            std::cout << "Succesfully loaded locations: " << c.name << "\n";
+            wait(20);
+        }
+        EMPTYSCREEN();
+    }
+
+    void setseed()
+    {
+        std::cout << "custom seed?\n";
+        std::vector<std::string> yesOrNo =
+        {
+            "Yes",
+            "No"
+        };
+
+        vectorCreation(yesOrNo.size());
+
+        for (int i = 0; i < yesOrNo.size(); i++)
+        {
+            std::cout << charPossibilities[i] << ". " << yesOrNo[i] << "\n";
+        }
+
+        printAscii("warrior.txt");
+
+        if (correctInput() == 'A')
+        {
+            std::cout << "Enter seed (8 numbers)\n";
+            std::cin >> seedValue;
+        }
+        else {
+            std::random_device dev;
+            std::mt19937 rng(dev());
+            std::uniform_int_distribution<std::mt19937::result_type> dist6(1, 99999999);
+
+            seedValue = dist6(rng);
+        }
+        std::cout << "Seed:" << std::setfill('0') << std::setw(8) << seedValue << "\n";
+        EMPTYSCREEN();
+    }
+
+    void setClass()
+    {
+        vectorCreation(characters.size());
+        std::cout << "Choose a character:\n";
+        for (int i = 0; i < charPossibilities.size(); i++)
+        {
+            std::cout << charPossibilities[i]
+                << ". "
+                << characters[i].name << RED << " [ max HP: "
+                << characters[i].hpMax << BLUE <<" | ATK: "
+                << characters[i].attack << RESET << "]\n";;
+        }
+
+        char playerChoice = correctInput();
+
+        int characterChoiceInt = static_cast<int>(playerChoice) - 'A';
+        //std::cout << characterChoiceInt;
+
+        std::cout << "You chose: " << characters[characterChoiceInt].name << "\n";
+
+        player = characters[characterChoiceInt];
+
+		playerCurrentHP = player.hpMax;
+    }
+
     void mainLoop()
     {
-        multipleWaySplit(3);
+        splitOptions(multipleWaySplit(1));
 		basicCombat();
     }
 
-    void multipleWaySplit(int AmountOfRoadChoices)
+    int multipleWaySplit(int AmountOfRoadChoices)
     {
-        amountOfChoices = AmountOfRoadChoices;
-        vectorCreation();
+        vectorCreation(AmountOfRoadChoices);
 
-        std::cout << "You arrive at a split in the road. There are " << amountOfChoices << " Possible ways. Which one will you take?\n";
+        std::cout << "You arrive at a split in the road. There are " << AmountOfRoadChoices << " Possible ways. Which one will you take?\n";
 
-        for (int i = 0; i < amountOfChoices; i++)
+        for (int i = 0; i < AmountOfRoadChoices; i++)
         {
-            std::cout << "Road " << charPossibilities[i] << ".\n";
+            std::cout << "Road " << charPossibilities[i] << " to " << locations[i].name << ".\n";
         }
 
         char roadChoiceMain = correctInput();
         std::cout << "You chose road: " << roadChoiceMain << "\n";
-        return;
+        return roadChoiceMain - 'A';
+    }
+
+    void splitOptions(int chosenRoad)
+    {
+        vectorCreation(locations[chosenRoad].possibleActions.size());
+        std::cout << "What do you want to do?\n";
+        for (int i = 0; i < locations[chosenRoad].possibleActions.size(); i++)
+        {
+            std::cout << charPossibilities[i] << ". " << locations[chosenRoad].possibleActions[i].nameOfAction << "\n";
+        }
+        char chosenOption = correctInput();
+        std::cout << "You chose option: " << chosenOption << "\n";
     }
 
     void basicCombat()
@@ -309,8 +395,7 @@ private:
             "Ability"
         };
 
-        amountOfChoices = playerCombatOptions.size();
-        vectorCreation();
+        vectorCreation(playerCombatOptions.size());
 
         std::cout << "It is your turn, choose your action:\n";
         for (int i = 0; i < playerCombatOptions.size(); i++)
